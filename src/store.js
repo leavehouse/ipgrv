261fcc6b601d25106de2e5d5b6e728a65ae2f08a
leavehouse <heavelouse@gmail.com>
2017-12-07 22:20:02 -0600

    Refactor view code into a separate file

--- src/store.js
+++ src/store.js
@@ -1,16 +1,3 @@
-/* operations for interacting with the...store? model?
- *  - initial one where we get the entire filetree, possibly sans leaves
- *     - though maybe see the git bomb article, or think about very large monorepos?
- *     - but this is perhaps okay as a first step
- *  - get blob data.
- *
- * actually, probably better to talk about different methods state actions will need
- *  - getDirectory: on seeing a CID for a commit object and a path for its associated file tree,
- *                  returns the children of the file tree node at the path (i.e. returns the
- *                  directory entries for the directory at the path)
- *  - getBlob: getDirectory but for navi
- */
-
 function getDirStructure(cid) {
   // TODO: actually make requests to IPFS
   return {
@@ -54,13 +41,6 @@ function isObject(x) {
 
 // `path` is an array of path segments
 export const getDirectory = ({ cid, path }) => {
-  /* For now only do tree views (blobs wont have clickable links):
-   *
-   * if `cid` is not the one currently stored
-   *   get its entire "directory structure" (the file tree sans leaves)
-   *
-   * navigate to the appropriate subtree, return children of subtree root
-   */
   if (cid !== cache.cid) {
     cache.dirStructure = getDirStructure(cid);
     cache.cid = cid;
---
+++ src/view.js
@@ -0,0 +1,97 @@
+import { h } from "hyperapp"
+import { Route, Link } from "@hyperapp/router"
+import { getDirectory } from "./store"
+
+const demoCommitHash = "z8mWaFhg8TJBrcjq3FtHq92Y6TsqzhNs7";
+
+export const mainView = state => actions =>
+  h('main', {class: 'container grid-lg'}, [
+    Route({ path: '/', render: Home }),
+    Route({ path: '/tree/:cid', render: Filetree, parent: true}),
+  ]);
+
+const Home = () =>
+  Link({ to: `/tree/${demoCommitHash}` }, 'Demo repo');
+
+const Filetree = ({ location, match }) => {
+  const treePath = (location.pathname.length === match.url.length
+                    ? '/'
+                    : location.pathname.substring(match.url.length));
+
+  const treePathArray = pathToArray(treePath);
+  return (
+    h('div', {}, [
+      h('h1', {}, 'Commit object CID: '+match.params.cid),
+      TreeBreadcrumb({ matchUrl: match.url, pathArray: treePathArray }),
+      TreeTable({ locationPathname: location.pathname, pathArray: treePathArray,
+                  cid: match.params.cid }),
+    ])
+  );
+}
+
+function makeBreadcrumbsLinkData(pathArray) {
+  let pathSegData = [];
+  let parentPath = '';
+  if (pathArray.length > 0) {
+    pathSegData.push({ segment: '/' })
+  }
+  for (var i = 0; i < pathArray.length; i++) {
+    const pathSeg = pathArray[i];
+    pathSegData.push({ segment: pathSeg, pathToParent: parentPath });
+    parentPath = parentPath+'/'+pathSeg;
+  }
+  return pathSegData;
+}
+
+const TreeBreadcrumb = ({ matchUrl, pathArray }) => {
+  const pathSegData = makeBreadcrumbsLinkData(pathArray);
+  // `matchUrl` = /tree/:cid, and each path segment's `pathToParent` field  = /some/path/here,
+  //  where `pathToParent` = <empty string> for the first path segment
+  return (
+    h('nav', { 'aria-label': 'breadcrumb', role: 'navigation'},
+      h('ol', { 'class': 'breadcrumb' },
+        pathSegData
+          .map((pathSeg, i) =>
+            breadcrumbSegment({ pathSeg, matchUrl, isLast: i === pathSegData.length - 1}))))
+  );
+};
+
+const breadcrumbSegment = ({pathSeg, matchUrl, isLast }) => {
+  return (
+    h('li', {'class': 'breadcrumb-item'},
+      (isLast
+       ? pathSeg.segment
+       : (pathSeg.segment === '/'
+         ? Link({ to: matchUrl },
+                h('i', {class: 'link fa fa-home', 'aria-label': 'Home'}))
+         : Link({ to: `${matchUrl}${pathSeg.pathToParent}/${pathSeg.segment}` },
+                pathSeg.segment))))
+  );
+};
+
+const TreeTable = ({ locationPathname, pathArray, cid }) => {
+  const entries = getDirectory({ cid: cid, path: pathArray });
+  const listItems = entries && entries.map(entry =>
+    h('tr', {},
+      h('td', {},
+        (entry.isDir
+         ? Link({ to: `${locationPathname}/${entry.name}` }, entry.name)
+         : entry.name))));
+
+  return (
+    h('table', {class: 'table table-striped'},
+      h('tbody', {}, listItems))
+  );
+}
+
+const pathToArray = path => {
+  if (path[0] !== '/') {
+    throw new Error('path must begin with "/"');
+  }
+  if (path === '/') {
+    return [];
+  } else {
+    // trim leading and trailing slashes first
+    return path.replace(/^\//, '').replace(/\/$/, '').split('/');
+  }
+};

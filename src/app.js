24c2dfa04e64a03f72170cede8734c2c0ccb3113
leavehouse <heavelouse@gmail.com>
2018-01-15 01:56:34 -0600

    Move styles.css to src directory

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import { app } from "hyperapp"
 import { location } from "hyperapp-hash-router"
-import { model } from "./model"
-import { mainView } from "./view"
 import "tachyons/css/tachyons.css"
+import { model } from "./model"
+import "./styles.css"
+import { mainView } from "./view"
 
 const {state, actions} = model;
 const appActions = app(state, actions, mainView, document.body);
